beea19b1d9584d7a2b752342b39570e9b220c1a8
Jean-Pascal MILCENT <jp.milcent@cbn-alpin.fr>
2020-08-13 18:20:04 +0200

    Feat web-srv/home/geonat: add config/custom files for GeoNature and Atlas

---
+++ web-srv/home/geonat/geonature/frontend/src/custom/components/introduction/introduction.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+import { AppConfig } from '@geonature_config/app.config';
+
+@Component({
+  selector: 'pnx-introduction',
+  styleUrls: ['introduction.component.scss'],
+  templateUrl: 'introduction.component.html'
+})
+export class IntroductionComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}

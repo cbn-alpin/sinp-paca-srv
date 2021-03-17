6fc91e4cef440e782d706c44a281b2e8217a950a
Jean-Pascal MILCENT <jp.milcent@cbn-alpin.fr>
2021-03-17 11:50:45 +0100

    Feat db-srv/backups: send email only on error with attached pg_dump log

--- db-srv/home/admin/bin/backup_gnatlas.sh
+++ db-srv/home/admin/bin/backup_gnatlas.sh
@@ -2,21 +2,49 @@
 # Encoding : UTF-8
 # Backup GeoNature-Atlas Postgresql Database
 
-BACKUP_DIR="/home/admin/backups/postgresql"
-BACKUP_NAME="$(date +%F)_gnatlas"
-BACKUP_PATH="${BACKUP_DIR}/${BACKUP_NAME}.sql"
+db_name="gnatlas"
+db_user="geonatatlas"
+db_host="localhost"
+db_port="5432"
 
-mkdir -p "${BACKUP_DIR}";
-cd "${BACKUP_DIR}"
+email_from="mailer@silen.eu"
+email_to="adminsys@silene.eu"
 
-pg_dump --file "${BACKUP_PATH}" \
-	--host "localhost" \
-	--port "5432" \
-	--username "geonatatlas" \
+bkp_dir="/home/admin/backups/postgresql"
+bkp_name="$(date +%F)_${db_name}"
+bkp_path="${bkp_dir}/${bkp_name}"
+bkp_log="/tmp/pgdump_${db_name}.log"
+
+
+echo "Start at $(date '+%Y-%m-%d %T')" > ${bkp_log}
+# Build and go to backup dir
+mkdir -p "${bkp_dir}"
+cd "${bkp_dir}"
+
+# Create dump
+pg_dump --file "${bkp_path}" \
+	--host "${db_host}" \
+	--port "${db_port}" \
+	--username "${db_user}" \
 	--verbose \
 	--format=p \
-	gnatlas
-tar -cvf "${BACKUP_NAME}.tar" -C "${BACKUP_DIR}" "${BACKUP_NAME}.sql"
+	"${db_name}" >> ${bkp_log} 2>&1
+# Save exit code of pg_dump process into variable
+dump_status=$?
 
-rm -f "${BACKUP_PATH}"
-find "${BACKUP_DIR}" -name "*_gnatlas.tar" -type f -mtime +365 -exec rm -f {} \;
+# Create a tar not compresseed (already done by dump)
+tar -cvf "${bkp_name}.tar" -C "${bkp_dir}" "${bkp_name}/"
+
+# Clean temp file and old dumps
+rm -fR "${bkp_name}/"
+find "${bkp_dir}" -name "*_${db_name}.tar" -type f -mtime +365 -exec rm -f {} \;
+
+echo "End at $(date '+%Y-%m-%d %T')" >> ${bkp_log}
+
+# Send email if something goes wrong with dump
+if [[ ${dump_status} -ne 0 ]] ; then
+	EMAIL="${email_from}"; echo "See attached log file." | mutt \
+		-s "[Cron] Backups ERROR ${dump_status} - DB ${db_name} - $(date '+%Y-%m-%d %T')" \
+		-a "${bkp_log}" -- \
+		"${email_to}"
+fi

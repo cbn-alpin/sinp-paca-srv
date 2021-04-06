859cbf119904143c59540a3846a8ea9e88f5633d
Jean-Pascal MILCENT <jp.milcent@cbn-alpin.fr>
2021-04-06 11:40:28 +0200

    Feat docker/wiki-sinp: use VERSION to check upgrade need

--- web-srv/docker/wiki-sinp.cbn-alpin.fr/dokuwiki/build/dokuwiki-entrypoint.sh
+++ web-srv/docker/wiki-sinp.cbn-alpin.fr/dokuwiki/build/dokuwiki-entrypoint.sh
@@ -2,15 +2,26 @@
 set -e
 
 # Copy Dokuwiki upgraded files into volume...
-if [[ -d "/tmp/dokuwiki-${DW_VERSION}" ]]; then
-	echo "/tmp/dokuwiki-${DW_VERSION} exists !"
-	'cp' -af /tmp/dokuwiki-${DW_VERSION}/* ${HTML_PATH}/
-	cd ${HTML_PATH}/
-	grep -Ev '^($|#)' ${HTML_PATH}/data/deleted.files | xargs -n 1 rm -fr
-	rm -fR /tmp/dokuwiki-${DW_VERSION}/
-	chown -R www-data: ${HTML_PATH}
+DW_VERSION_CURRENT=$(cat "${HTML_PATH}/VERSION")
+CYellow="\\033[1;33m"
+CGreen="\\033[1;32m"
+CRed="\\033[1;31m"
+CReset="\\033[0;39m"
+if [[ -d "/tmp/dokuwiki-${DW_VERSION_DATE}" ]]; then
+	echo -e ${CYellow}"/tmp/dokuwiki-${DW_VERSION_DATE} exists !"${CReset}
+	if grep -q '${DW_VERSION}' "${HTML_PATH}/VERSION"; then
+		echo -e ${CGreen}'Dokuwiki already in version ${DW_VERSION}. Dokuwiki will not be upgraded !'${CReset}
+	else
+		echo -e ${CYellow}'Dokuwiki upgrading to ${DW_VERSION} !'${CReset}
+		'cp' -af /tmp/dokuwiki-${DW_VERSION_DATE}/* ${HTML_PATH}/
+		cd ${HTML_PATH}/
+		grep -Ev '^($|#)' ${HTML_PATH}/data/deleted.files | xargs -n 1 rm -fr
+		rm -fR /tmp/dokuwiki-${DW_VERSION_DATE}/
+		chown -R www-data: ${HTML_PATH}
+	fi
 else
-	echo "/tmp/dokuwiki-${DW_VERSION} NOT exists !"
+	echo -e ${CGreen}"/tmp/dokuwiki-${DW_VERSION_DATE} NOT exists."${CReset}
+	echo -e ${CGreen}"Dokuwiki already in version ${DW_VERSION_CURRENT}. Dokuwiki will not be upgraded !"${CReset}
 fi
 
 exec "$@"

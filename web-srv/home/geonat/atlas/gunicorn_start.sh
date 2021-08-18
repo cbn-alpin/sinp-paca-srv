6ba2542cf5ab0cee15502b0fdbae88fc2b732cc9
Jean-Pascal MILCENT <jp.milcent@cbn-alpin.fr>
2021-08-18 11:53:30 +0200

    Feat web-srv: add gunicorn options for statsd

---
+++ web-srv/home/geonat/atlas/gunicorn_start.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+FLASKDIR=$(readlink -e "${0%/*}")
+
+. "$FLASKDIR"/atlas/configuration/settings.ini
+
+echo "Starting $app_name"
+echo "$FLASKDIR"
+
+# activate the virtualenv
+cd $FLASKDIR/$venv_dir
+source bin/activate
+
+export PYTHONPATH=$FLASKDIR:$PYTHONPATH
+
+# Start your unicorn
+exec gunicorn initAtlas:app \
+	--statsd-host=localhost:8125 \
+	--statsd-prefix=atlas \
+	--error-log $FLASKDIR/log/errors_atlas.log \
+	--pid="${app_name}.pid" \
+	--timeout=${gun_timeout} \
+	-w "${gun_num_workers}" \
+	-b "${gun_host}:${gun_port}" \
+	-n "${app_name}"
